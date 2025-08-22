fee6dfa4af1280bccdd5c51d030b0c26d844a493
lzfDream <1719472077@qq.com>
2025-08-22 16:50:18 +0800

    update dockerfile

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /app/tools-config
-cp bash/.bashrc ~/.bashrc
-cat zsh/.zshrc >> ~/.zshrc
-cp -r vim/autoload ~/.vim
-cp vim/.vimrc ~/.vimrc
-cp git/.gitconfig ~/.gitconfig
-
-exec "$@"
